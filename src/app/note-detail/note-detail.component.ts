8031acdc058f66bccbbba018bb05867cf2e6f07f
Thibaut Stachnick <thibautstachnick@gmail.com>
2023-09-15 15:56:32 +0200

    add view on note and delete button

---
+++ src/app/note-detail/note-detail.component.ts
@@ -0,0 +1,26 @@
+import {Component, inject} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ActivatedRoute, Router, RouterLink} from "@angular/router";
+import {NOTES} from "../../notes";
+
+@Component({
+  selector: 'app-note-detail',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './note-detail.component.html',
+  styleUrls: ['./note-detail.component.css']
+})
+export class NoteDetailComponent {
+  activeRoute = inject(ActivatedRoute);
+  router = inject(Router)
+  id = Number(this.activeRoute.snapshot.paramMap.get('id'));
+  note = NOTES.find((i) => i.id === this.id);
+  index: number =0;
+  removeNote(){
+    if (this.note){
+      this.index = NOTES.indexOf(this.note,0)
+    }
+    NOTES.splice(this.index,1)
+    this.router.navigateByUrl("/")
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { AppComponent } from './app/app.component';
 import {NotesListComponent} from "./app/notes-list/notes-list.component";
 import {provideRouter, Routes} from "@angular/router";
 import {AddNoteComponent} from "./app/add-note/add-note.component";
+import {NoteDetailComponent} from "./app/note-detail/note-detail.component";
 
 const routes : Routes = [
   {path:"",component:NotesListComponent},
-  {path:"new",component:AddNoteComponent}
+  {path:"new",component:AddNoteComponent},
+  {path:"note/:id", component:NoteDetailComponent}
 ]
 
 bootstrapApplication(AppComponent,{
